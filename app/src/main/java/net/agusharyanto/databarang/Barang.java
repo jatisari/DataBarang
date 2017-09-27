ee6f57d727f63b8fa799bc2e7d1a28d749098d3a
Super Writer <jatisari.innovation@gmail.com>
2017-09-27 11:41:11 +0700

    Latihan database

---
+++ app/src/main/java/net/agusharyanto/databarang/Barang.java
@@ -0,0 +1,13 @@
+package net.agusharyanto.databarang;
+
+/**
+ * Created by agus on 9/27/17.
+ */
+
+public class Barang {
+
+    private String id="";
+    private String kode="";
+    private String nama="";
+    private String barang="";
+}
