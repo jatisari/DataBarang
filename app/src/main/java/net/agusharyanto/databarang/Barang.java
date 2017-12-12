3e8b4c6c6b715d812fac59b26b40abdd17e904ff
agus <jatisari.innovation@gmail.com>
2017-12-12 16:35:31 +0700

    Latihan database

--- app/src/main/java/net/agusharyanto/databarang/Barang.java
+++ app/src/main/java/net/agusharyanto/databarang/Barang.java
@@ -1,13 +1,57 @@
 package net.agusharyanto.databarang;
 
+import java.io.Serializable;
+
 /**
  * Created by agus on 9/27/17.
  */
 
-public class Barang {
+public class Barang implements Serializable{
 
     private String id="";
     private String kode="";
     private String nama="";
-    private String barang="";
+    private String harga ="";
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    @Override
+    public String toString() {
+        return "Barang{" +
+                "id='" + id + '\'' +
+                ", kode='" + kode + '\'' +
+                ", nama='" + nama + '\'' +
+                ", harga='" + harga + '\'' +
+                '}';
+    }
 }
